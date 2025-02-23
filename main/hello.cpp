d810ce3c5fdef5f44713ccdc9d443515105f9391
capnmav77 <its.mastermind77@gmail.com>
2025-02-23 21:20:17 +0530

    mod cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this againx2 \n";
+  cout << "I have successfully built and run this \n";
   return 0;
 }
